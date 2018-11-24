57c3562ca3c89bc28d50dab44d00eba6f14bbb96
rakhmadbudiono <44491809+rakhmadbudiono@users.noreply.github.com>
2018-11-24 17:36:46 +0700

    help debug enemyMovement

--- enemy.pl
+++ enemy.pl
@@ -12,3 +12,33 @@ mappingWeapon(X, 1) :- asserta(weapon(X, kunaiThrower)), !.
 mappingWeapon(X, 2) :- asserta(weapon(X, shurikenThrower)), !.
 mappingWeapon(X, 3) :- asserta(weapon(X, rasengan)), !.
 mappingWeapon(X, 4) :- asserta(weapon(X, sexyNoJutsu)), !.
+
+enemyMovement(X, K, L) :- mappingEnemyMovement(X, 2, K, L).
+
+mappingEnemyMovement(_, 1, _, _) :- !.
+mappingEnemyMovement(X, 2, C, D) :-  location(self, A, B), retract(location(X, C, D)), A > C, B > D,
+  Cnew is C+1, Dnew is D+1, asserta(location(X, Cnew, Dnew)), !.
+mappingEnemyMovement(X, 2, C, D) :-  location(self, A, B), retract(location(X, C, D)), A > C, B == D,
+  Cnew is C+1, Dnew is D, asserta(location(X, Cnew, Dnew)), !.
+mappingEnemyMovement(X, 2, C, D) :-  location(self, A, B), retract(location(X, C, D)), A > C, B < D,
+  Cnew is C+1, Dnew is D-1, asserta(location(X, Cnew, Dnew)), !.
+mappingEnemyMovement(X, 2, C, D) :-  location(self, A, B), retract(location(X, C, D)), A == C, B > D,
+  Cnew is C, Dnew is D+1, asserta(location(X, Cnew, Dnew)), !.
+mappingEnemyMovement(X, 2, C, D) :-  location(self, A, B), retract(location(X, C, D)), A == C, B == D,
+  Cnew is C, Dnew is D, asserta(location(X, Cnew, Dnew)), !.
+mappingEnemyMovement(X, 2, C, D) :-  location(self, A, B), retract(location(X, C, D)), A == C, B < D,
+  Cnew is C, Dnew is D-1, asserta(location(X, Cnew, Dnew)), !.
+mappingEnemyMovement(X, 2, C, D) :-  location(self, A, B), retract(location(X, C, D)), A < C, B > D,
+  Cnew is C+1, Dnew is D+1, asserta(location(X, Cnew, Dnew)), !.
+mappingEnemyMovement(X, 2, C, D) :-  location(self, A, B), retract(location(X, C, D)), A < C, B == D,
+  Cnew is C+1, Dnew is D, asserta(location(X, Cnew, Dnew)), !.
+mappingEnemyMovement(X, 2, C, D) :-  location(self, A, B), retract(location(X, C, D)), A < C, B < D,
+  Cnew is C+1, Dnew is D-1, asserta(location(X, Cnew, Dnew)), !.
+
+attackPlayer(X) :- location(X, A, B), location(self, A, B), retract(health(self, H)), weapon(X, W),
+  weaponList(W, D), HP is H-D, asserta(health(self, HP)), write(X), write(" baru saja menyerangmu dengan "),
+  write(W), write(". Darahmu berkurang sebanyak "), write(D).
+
+attackPlayer(_).
+
+enemiesTurn :- forall(enemy(X), forall(location(X, A, B), (enemyMovement(X, A, B), attackPlayer(X)))), !.
